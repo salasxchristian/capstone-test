29f9d483b4f4ae7db7de1902cbb0cfb1f5f0723b
christiansalasdev <chris@christiansalas.dev>
2024-03-27 08:49:32 -0700

    update settings

--- acmeportal/settings.py
+++ acmeportal/settings.py
@@ -32,7 +32,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*.onrender.com', 'localhost']
 
 
 LOGOUT_REDIRECT_URL = "/accounts/login"
